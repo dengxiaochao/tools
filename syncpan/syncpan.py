2334d1cb6caed3ab098ce7001a18f18b588568b5
dengxiaochao <dengxiaochao@baidu.com>
2017-12-03 21:04:59 +0800

    basic structure

--- syncpan/syncpan.py
+++ syncpan/syncpan.py
@@ -1,3 +1,4 @@
+import time
 from baidupcsapi import PCS
 
 class SyncPan(object):
@@ -6,9 +7,34 @@ class SyncPan(object):
     Parse remote paths' incremental dir files, download to local
     """
     pcs = None
+    sync_paths = None
+    period = 10
 
-    def _init_(self, username, password, sync_paths=None):
-        try:
-            pcs = PCS(username, password)
-            quota = pcs.quota().
+    def _init_(self, username, password, sync_paths=None, period=10):
+        self.pcs = PCS(username, password)
+        if sync_paths is not None:
+            self.sync_paths = sync_paths
+        self.period = period
+
+    def run(self):
+        next_run = time.time
+        while True:
+            now = time.time
+            if now < next_run:
+                time.sleep(0.1)
+                continue
+            next_run = now + self.period
+            self.sync_all()
+            pass
+        pass
+
+    def sync_all(self):
+        for path in self.sync_paths:
+            self.sync_one(path)
+            pass
+        pass
+
+    def sync_one(self, path):
+        pass
+
 
